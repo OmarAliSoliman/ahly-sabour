948c8f0255d93b2371d051592e1ee7de86c52c7e
Omar Soliman <omar.soliman@viola.ae>
2023-12-06 21:01:32 +0200

    page

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,95 @@ $(document).ready(function () {
     $(".nice_select").niceSelect();
   }
 
+  if ($(".projects_cards_slider").length) {
+    $(".projects_cards_slider").slick({
+      slidesToShow: 1.5,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      speed: 300,
+      infinite: false,
+      autoplaySpeed: 5000,
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 1.1,
+          },
+        },
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 4,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2.2,
+            centerMode: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            centerMode: false,
+          },
+        },
+      ],
+    });
+  }
 
-  // sunstan_our_partners_erp_slider
+
+  if ($(".avilible_projects_cards_slider").length) {
+    $(".avilible_projects_cards_slider").slick({
+      slidesToShow:5.7,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      speed: 300,
+      infinite: true,
+      autoplaySpeed: 5000,
+      centerMode: true,
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 2400,
+          settings: {
+            slidesToShow: 4.6,
+          },
+        },
+        {
+          breakpoint: 1800,
+          settings: {
+            slidesToShow: 3.7,
+          },
+        },
+        {
+          breakpoint: 1450,
+          settings: {
+            slidesToShow: 3.3,
+            centerMode: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            centerMode: false,
+          },
+        },
+      ],
+    })
+  }
 
 
+  $(".nav-pills .nav-item .nav-link").on('shown.bs.tab', function () {
+    $('.projects_cards_slider').slick('setPosition');
+    AOS.refresh();
+  });
 
   AOS.init();
 });
